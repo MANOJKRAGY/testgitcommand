6217196a602bcf469c8f99bca7ddca2a4c789d61
Manoj kumar <manojkragy@gmail.com>
2024-12-10 17:02:17 +0530

    merged the service from main and amazon from amazon

--- abc.java
+++ abc.java
@@ -3,3 +3,5 @@ I am 30 years old.
 I have completed my Btech.
 I am currently working in a IT company.
 I have swithced to a Service based company.
+I have switched to an another company of amazon.
+My PERKS are as follows.
