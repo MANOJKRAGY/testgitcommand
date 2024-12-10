10438b3e791d4c8b7162648964a319d0bf76dadd
Manoj kumar <manojkragy@gmail.com>
2024-12-10 16:38:15 +0530

    switched to amazon

--- abc.java
+++ abc.java
@@ -2,4 +2,5 @@ My name is manoj kumar.
 I am 30 years old.
 I have completed my Btech.
 I am currently working in a IT company.
-
+I have switched to an another company of amazon.
+My PERKS are as follows.
